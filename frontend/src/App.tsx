3a1c8902149f582dcee6621d2d38ad50d1f34453
Thiago Gonzalez <thgonzalez@frba.utn.edu.ar>
2025-04-16 13:10:12 -0300

    Publicidad nivel 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,28 +5,35 @@ import Header from './components/header/Header'
 import IndiceCategoriasBanner from './components/catalogo/IndiceCategoriasBanner'
 import CategoriasRecomendadasBanner from "./components/catalogo/CategoriasRecomendadasBanner";
 import PublicidadBanner from "./components/catalogo/PublicidadNivel2Banner";
+import PublicidadNivel1 from "./components/catalogo/PublicidadNivel1Banner";
 
 import Publicidad from "./assets/publicidades/publicidad1.png";
+import Publicidad1 from "./assets/publicidades/publicidad2.png";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      
+
+      {/* Publicidad que ocupa todo el ancho */}
+      <PublicidadNivel1 imagen={Publicidad1} link="/ofertas" alt="PublicidadNivel1" />
+
       <main className="flex-grow bg-[#FFCE83]">
-      <div className="container mx-auto py-8">
-          {/* Tu contenido va acá */}
+        <div className="container mx-auto py-8">
+          {/* Contenido limitado al ancho */}
           <CategoriasRecomendadasBanner />
           <PublicidadBanner 
-           imagen={Publicidad}
-           link="https://tusitio.com/productos"
-           alt="Conoce todos los productos"/>
+            imagen={Publicidad}
+            link="https://tusitio.com/productos"
+            alt="Conoce todos los productos"
+          />
           <IndiceCategoriasBanner />
         </div>
       </main>
+
       <Footer />
     </div>
   );
-};
+}
 
 export default App; 
--- frontend/src/components/catalogo/PublicidadNivel1Banner.tsx
+++ frontend/src/components/catalogo/PublicidadNivel1Banner.tsx
@@ -1,1 +1,27 @@
-export {}
+import React from "react";
+
+type PublicidadNivel1Props = {
+    imagen: string;
+    alt?: string;
+    link?: string;
+  };
+
+const PublicidadNivel1: React.FC<PublicidadNivel1Props> = ({ imagen, link = "#", alt = "" }) => {
+    return (
+        <a href={link} className="block w-full">
+            <div className="relative w-full max-w-full h-auto overflow-hidden">
+                {/* Imagen de fondo a pantalla completa */}
+                <img
+                    src={imagen}
+                    alt={alt}
+                    className="w-full object-cover"
+                />
+
+                {/* Degradado inferior que se funde con el fondo */}
+                <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-b from-transparent to-[#FFCE83] pointer-events-none" />
+            </div>
+        </a>
+    );
+};
+
+export default PublicidadNivel1;
